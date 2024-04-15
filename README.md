de9eab626d5ccf9bce806aef265e4da3a99264fa
Kumar Gaurav <kmrgaurav33@gmail.com>
2024-04-15 17:48:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# @kumargaurav/hello
+
+My first jsr package.
+
+## Usuage
+
+Installation
+`npx jsr add @kumargaurav/hello`
+
+Example
+
+```js
+import { hello } from "@kumargaurav/hello";
+
+hello('your-name')
+```
+
+### License
+
+MIT
---
+++ mod.ts
@@ -0,0 +1,16 @@
+/**
+ * This function greets user via console.log
+ * 
+ * @param name User's name
+ * @returns Greeting message
+ * 
+ * example
+ * ```js
+ * hello('Mithila Palkar')
+ * ```
+ *  */
+export function hello (name: string): string {
+  console.log(`Hello ${name} 👋🏻`)
+
+  return `Hello ${name} 👋🏻`
+}
